eb7583c096e653ae1fb89266a8e1d84aee822e7e
cavacado <ngzhaoloong@gmail.com>
2021-02-14 17:29:54 +0800

    refactor / delete useless code
    update README.md

--- README.md
+++ README.md
@@ -1,47 +1,23 @@
-# NHKforKindle
-
+# Easy NHK in HTML
 ## Description:
 
-If someone is having a habit of reading the japanese news from NHK every day, here is a script that I modified to download todays news from NHK Easy in a single clean HTML and than convert it to MOBI format for your Kindle.
+If someone is having a habit of reading the japanese news from NHK every day, here is a script that I modified to download todays news from NHK Easy in a single clean `HTML`.
 
-Here are the scripts, download the files in a folder and ~~start it with bash file kindle.sh~~. You will need to have in the same folder Kindlegen (https://www.amazon.com/gp/feature.html?docId=1000765211) and also Python3 installed. 
-(If you are in Windows OS probably you will have to run manually the python file and then kindlegen with the created .opf file.) 
-
-The MOBI file will have:   
+The `HTML` file will have:   
 - ~~all 5 news for today in one file~~ 
 - depending on `<mth>` specified, will parse the `<mth>`'s news 
 - vertical text   
 - right to left page flipping   
 - furigana   
-- correct word recognising for the Kindle japanese dictionaries (if you have paperwhite) 
-- shell scripting TODO
 
-## MODIFIED THE CODE FROM THIS REPO: 
-(https://github.com/vebaev/NHKforKindle/blob/master/README.md)
-
-Changed it to parse monthly instead of daily
-does not support shell script yet, will do so soon
-
-TO USE:
+## Usage:
 
 ```shell
+pip3 install <dependencies>
 python3 nhk-easy.py <mth>
 
 # where <mth> is the specified mth 
 # year is calculated using the sys clock
 ```
-
-output of the program should create a dir where the script was ran.
-
-eg: if you ran the python3 script in `~` (home dir for linux/macOS), it should create a folder `yyyy_mm` and would create 2 other files inside the dir with the extensions `.opf` and `.html` respectively.
-
-In order to convert it into a `.mobi` file, you would need `kindlegen`, im not going to explain the how to here, search the interwebs for more information.
-
-to be precise run the code
-```
-python3 nhk-easy.py <mth>
-
-// then after the operation is done, run the code
-
-kindlegen <xxxx.opf>
-```
+## References:
+- https://github.com/vebaev/NHKforKindle/blob/master/README.md
--- nhk-easy.py
+++ nhk-easy.py
@@ -1,7 +1,6 @@
 import requests
 import sys
 import json
-import codecs
 import os
 import re
 from bs4 import BeautifulSoup
@@ -10,15 +9,16 @@ from datetime import date
 
 def main():
 
-    r = requests.get('http://www3.nhk.or.jp/news/easy/news-list.json')
+    r = requests.get("http://www3.nhk.or.jp/news/easy/news-list.json")
     # utf-8-sig better at decoding than utf-8.
-    r.encoding = 'utf-8-sig'
+    r.encoding = "utf-8-sig"
     # Since the url we are using is a json file, we need
     # parse it using json. response.text contains content in unicode.
     # Load the content of the response and convert it to something
     # python can understand.
     o = json.loads(r.text)
     parse(o, sys.argv[1])
+
 
 # basically filtering out other months not specified
 
@@ -29,7 +29,7 @@ def parse(o, mth):
         # Regex match for date object: digits[0-9] + minus and all following ones
         # + month + -digits[0-9] and all following digits.
         # eg. 2018-08-21 = (\d+)(-+)month(-\d+).
-        regex = r'(\d+)' + r'(-+)' + re.escape(str(mth)) + r'(-\d+)'
+        regex = r"(\d+)" + r"(-+)" + re.escape(str(mth)) + r"(-\d+)"
         x = re.search(regex, k)
 
         if x:
@@ -45,83 +45,75 @@ def parse(o, mth):
 def parseMonth(dic, mth):
     # naming the output folder based on sys.argv and current year
     mth = str(mth)
-    yr = ''.join(date.today().strftime("%Y-%m-%d").split('-')[0:1])
-    join_str = yr + '_' + mth
+    yr = "".join(date.today().strftime("%Y-%m-%d").split("-")[0:1])
+    join_str = yr + "_" + mth
     save_path = os.getcwd()
-    output = save_path + '/' + join_str + '/' + join_str + '.html'
+    output = save_path + "/" + join_str + "/" + join_str + ".html"
     folder = join_str
 
     if os.path.isdir(folder) == False:
         os.makedirs(folder)
-        print("Directory \"" + folder + "/\" created")
+        print('Directory "' + folder + '/" created')
     elif os.listdir(folder):
-        print("Directory \"" + folder + "/\" exists!\n\nAbort.\n")
+        print('Directory "' + folder + '/" exists!\n\nAbort.\n')
         return
 
     items = []
     content = []
 
-    # O(n^2) 😱
-    # if deepak was here, he'll probably be squirming now
-    # luckily he isnt
-    for k, v in dic.items():
+    for _, v in dic.items():
         for i in v:
             item = parseNews(i)
             items.append(item)
             content.append(item["content"])
 
+    # so that the articles are arranged from oldest to newest
     content.reverse()
 
     with open(output, "w") as f:
         print('<?xml version="1.0" encoding="UTF-8" ?>', file=f)
         print("<!DOCTYPE html>", file=f)
         print("<html lang='ja'>", file=f)
-        print('<head><meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8" >', file=f)
-        print('<style type="text/css">body { margin-left: 1em; margin-right: 1em; margin-top: 2em; margin-bottom: 2em; writing-mode:tb-rl; -epub-writing-mode: vertical-rl; -webkit-writing-mode: vertical-rl; line-break: normal; -epub-line-break: normal; -webkit-line-break: normal; color: #eee; font-size: larger; background: #111; line-height: 200%; font-family: "Hiragino Sans", sans-serif; } p { text-indent: 1em; font-size: medium } h1 { font-weight: bold; font-size: large; }</style>', file=f)
+        print(
+            '<head><meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8" >',
+            file=f,
+        )
+        print(
+            '<style type="text/css">body { margin-left: 1em; margin-right: 1em; margin-top: 2em; margin-bottom: 2em; writing-mode:tb-rl; -epub-writing-mode: vertical-rl; -webkit-writing-mode: vertical-rl; line-break: normal; -epub-line-break: normal; -webkit-line-break: normal; color: #eee; font-size: larger; background: #111; line-height: 200%; font-family: "Hiragino Sans", sans-serif; } p { text-indent: 1em; font-size: medium } h1 { font-weight: bold; font-size: large; }</style>',
+            file=f,
+        )
         print("</head>", file=f)
         print("<body>", file=f)
         print("<br />".join(content), file=f)
         print("</body>", file=f)
         print("</html>", file=f)
-        print("File \"" + output + "\" created")
-
-        # dead code, for generation of a .mobi for kindle paper white
-        file = open(save_path + '/' + join_str + '/' + join_str + '.opf', "w")
-        file.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?><package version=\"3.0\" xmlns=\"http://www.idpf.org/2007/opf\"         unique-identifier=\"BookId\"> <metadata xmlns:dc=\"http://purl.org/dc/elements/1.1/\"           xmlns:dcterms=\"http://purl.org/dc/terms/\">   <dc:title>NHK ニュース・読み物・" + join_str +
-                   "</dc:title>    <dc:contributor>NHK</dc:contributor>   <dc:language>ja</dc:language>   <dc:publisher>NHK</dc:publisher> </metadata> <manifest>  <item id=\"titlepage\" href=\"" + output + "\" media-type=\"application/xhtml+xml\" /> </manifest> <spine toc=\"tocncx\" page-progression-direction=\"rtl\">  <itemref idref=\"titlepage\" /> </spine></package>")
-        print("File \"" + save_path + '/' + join_str +
-              '/' + join_str + '.opf' + "\" created")
-        file.close()
+        print('File "' + output + '" created')
 
     print("The month's news were downloaded from NHK.")
 
 
 def parseNews(news):
-    news_id = news['news_id']
-    news_time = news['news_prearranged_time'].replace(':', '-')
-    title = news['title']
-    title_ruby = news['title_with_ruby']
-    news_uri = 'http://www3.nhk.or.jp/news/easy/' + \
-        str(news_id) + '/' + str(news_id) + '.html'
+    news_id = news["news_id"]
+    title = news["title"]
+    news_uri = (
+        "http://www3.nhk.or.jp/news/easy/" + str(news_id) + "/" + str(news_id) + ".html"
+    )
 
     r = requests.get(news_uri)
-    r.encoding = 'utf-8'
+    r.encoding = "utf-8"
 
-    soup = BeautifulSoup(r.text, 'html.parser')
-    # https://www.crummy.com/software/BeautifulSoup/bs4/doc/#find
-    date = soup.find('p', attrs={'id': 'js-article-date'})
-    title = soup.find(
-        'h1', attrs={'class': 'article-main__title'})  # .find('h2')
-    article = soup.find('div', attrs={'id': 'js-article-body'})
+    soup = BeautifulSoup(r.text, "html.parser")
+    date = soup.find("p", attrs={"id": "js-article-date"})
+    title = soup.find("h1", attrs={"class": "article-main__title"})
+    article = soup.find("div", attrs={"id": "js-article-body"})
 
-    for a in article.findAll('a'):
-        # https://www.crummy.com/software/BeautifulSoup/bs4/doc/#unwrap
+    for a in article.findAll("a"):
         a.unwrap()
-        voice = {}
 
-    return {
-        "content": str(title) + str(date) + str(article)
-    }
+    for img in article.findAll("img"):
+        img.decompose()
+
+    return {"content": str(title) + str(date) + str(article)}
 
 
 main()
